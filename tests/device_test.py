6f9f963618dd844f1c8139122b1695e5b4096429
Joe Bowser <bowserj@gmail.com>
2024-11-02 14:18:58 -0700

    Tests that should be failing are failing

--- tests/device_test.py
+++ tests/device_test.py
@@ -1,34 +1,65 @@
 import unittest
-from  import Device, DeviceManager
+from daypack import Device, DeviceManager
 
 class TestDevice(unittest.TestCase):
     def setUp(self):
-        self.device = Device()
+        self.device = Device('Android', 'emualtor-5554')
 
     def test_initialization(self):
-        self.assertIsNotNone(self.device)
-        self.assertEqual(self.device.id, "")
-        self.assertEqual(self.device.name, "")
-        self.assertEqual(self.device.type, "")
-        self.assertEqual(self.device.status, "disconnected")
+        device_id = "test123"
+        device_type = "Android"
+        device = Device(device_type, device_id)
+        self.assertEqual(device.deviceId, device_id)
+        self.assertEqual(device.deviceType, device_type)
 
-    def test_set_device_info(self):
-        test_id = "device123"
-        test_name = "Test Device"
-        test_type = "ios"
-        
-        self.device.set_device_info(test_id, test_name, test_type)
-        
-        self.assertEqual(self.device.id, test_id)
-        self.assertEqual(self.device.name, test_name)
-        self.assertEqual(self.device.type, test_type)
+    def test_android_project_setup(self):
+        device = Device("Android", "test123")
+        device.setup_android_project()
+        self.assertTrue(device.ANDROID_PROJECT_PATH.startswith("/tmp"))
 
-    def test_connect_disconnect(self):
-        self.device.connect()
-        self.assertEqual(self.device.status, "connected")
-        
-        self.device.disconnect()
-        self.assertEqual(self.device.status, "disconnected")
+    def test_ios_project_setup(self):
+        device = Device("iOS", "test123") 
+        device.setup_ios_project()
+        self.assertTrue(device.IOS_PROJECT_PATH.startswith("/tmp"))
+
+    def test_set_android_launch_path(self):
+        device = Device("Android", "test123")
+        device.setup_android_project()
+        test_uri = "https://test.com"
+        device.set_android_launch_path(test_uri)
+        # Verify strings.xml was updated
+        import xml.etree.ElementTree as ET
+        strings_xml = os.path.join(device.ANDROID_PROJECT_PATH, "res", "values", "strings.xml")
+        tree = ET.parse(strings_xml)
+        root = tree.getroot()
+        launch_url = root.find("string[@name='launch_url']").text
+        self.assertEqual(launch_url, test_uri)
+
+    def test_set_ios_launch_path(self):
+        device = Device("iOS", "test123")
+        device.setup_ios_project() 
+        test_uri = "https://test.com"
+        device.set_ios_launch_path(test_uri)
+        # Verify config.json was updated
+        import json
+        config_path = os.path.join(device.IOS_PROJECT_PATH, "config.json")
+        with open(config_path) as f:
+            config = json.load(f)
+        self.assertEqual(config["webviewUrl"], test_uri)
+
+    def test_build(self):
+        device = Device("Android", "test123")
+        test_uri = "https://test.com"
+        device.build(test_uri)
+        # Verify Android project was setup and launch path set
+        self.assertTrue(device.ANDROID_PROJECT_PATH.startswith("/tmp"))
+        import xml.etree.ElementTree as ET
+        strings_xml = os.path.join(device.ANDROID_PROJECT_PATH, "res", "values", "strings.xml") 
+        tree = ET.parse(strings_xml)
+        root = tree.getroot()
+        launch_url = root.find("string[@name='launch_url']").text
+        self.assertEqual(launch_url, test_uri)
+
 
 class TestDeviceManager(unittest.TestCase):
     def setUp(self):
@@ -36,44 +67,11 @@ class TestDeviceManager(unittest.TestCase):
 
     def test_initialization(self):
         self.assertIsNotNone(self.device_manager)
-        self.assertEqual(len(self.device_manager.devices), 0)
 
-    def test_add_remove_device(self):
-        test_device = Device()
-        test_device.set_device_info("test123", "Test Device", "ios")
-        
-        # Test adding device
-        self.device_manager.add_device(test_device)
-        self.assertEqual(len(self.device_manager.devices), 1)
-        
-        # Test removing device
-        self.device_manager.remove_device(test_device.id)
-        self.assertEqual(len(self.device_manager.devices), 0)
+    def test_devices_empty(self):
+        devices = self.device_manager.devices()
+        self.assertEqual(len(devices), 0)
 
-    def test_get_device(self):
-        test_device = Device()
-        test_device.set_device_info("test123", "Test Device", "ios")
-        self.device_manager.add_device(test_device)
-        
-        retrieved_device = self.device_manager.get_device("test123")
-        self.assertIsNotNone(retrieved_device)
-        self.assertEqual(retrieved_device.id, "test123")
-        
-        # Test getting non-existent device
-        non_existent = self.device_manager.get_device("fake_id")
-        self.assertIsNone(non_existent)
-
-    def test_get_all_devices(self):
-        test_device1 = Device()
-        test_device1.set_device_info("test1", "Test Device 1", "ios")
-        test_device2 = Device()
-        test_device2.set_device_info("test2", "Test Device 2", "ios")
-        
-        self.device_manager.add_device(test_device1)
-        self.device_manager.add_device(test_device2)
-        
-        devices = self.device_manager.get_all_devices()
-        self.assertEqual(len(devices), 2)
 
 if __name__ == '__main__':
     unittest.main()
